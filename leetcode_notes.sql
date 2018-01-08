e3c0a1857b40195de091d635a6111b03408503ba
Steve Qi Wang <steve.q.wang@gmail.com>
2018-01-07 18:11:03 -0800

    add new notes

--- leetcode_notes.sql
+++ leetcode_notes.sql
@@ -2,6 +2,12 @@
 UPDATE salary 
 SET sex = IF(sex = 'f', 'm','f')
 
-# 595. Big Countries
+# 595. Big Countries - https://leetcode.com/problems/big-countries/description/
 SELECT name, population, area FROM World
   WHERE area > 3000000 OR population > 25000000
+
+# on how to find odd/even numbers
+#620. Not Boring Movies - https://leetcode.com/problems/not-boring-movies/description/
+SELECT * FROM cinema 
+WHERE (id % 2) <> 0 AND description <>'boring'
+ORDER BY rating DESC
